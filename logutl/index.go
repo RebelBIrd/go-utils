9457fff307cd9889e1f2d92d2b7fe62c2913bbfe
qinyuanmao <qinyuanmao@hotmail.com>
2019-09-05 16:03:02 +0800

    fix log format

--- logutl/index.go
+++ logutl/index.go
@@ -1,27 +1,86 @@
 package logutl
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gookit/color"
+	"github.com/qinyuanmao/go-utils/timeutl"
+	"runtime"
+	"strconv"
+	"time"
+)
 
 func Error(message ...interface{}) {
-	fmt.Println(Red("--------------Error Start--------------"))
-	fmt.Println(Red(message))
-	fmt.Println(Red("--------------Error End--------------"))
+	fmt.Println()
+	color.Red.Println("------------------------------------------------ERROR-----------------------------------------------")
+	color.Red.Println("Time: " + timeutl.Time(time.Now()).String())
+	for i := 1; i <= 5; i ++ {
+		pc, _, line, ok := runtime.Caller(i)
+		f := runtime.FuncForPC(pc)
+		if ok && f.Name() != "runtime.goexit" {
+			color.Red.Println("Caller Skip" + strconv.Itoa(i) + ": " + f.Name() + ":" + strconv.Itoa(line) + "")
+		} else {
+			break
+		}
+	}
+	color.Red.Println("----------------------------------------------------------------------------------------------------")
+	color.Red.Println(message)
+	color.Red.Println("----------------------------------------------------------------------------------------------------")
+	fmt.Println()
 }
 
 func Debug(message ...interface{}) {
-	fmt.Println(Green("--------------Debug Start--------------"))
-	fmt.Println(Green(message))
-	fmt.Println(Green("--------------Debug End--------------"))
+	fmt.Println()
+	color.Green.Println("------------------------------------------------DEBUG-----------------------------------------------")
+	color.Green.Println("Time: " + timeutl.Time(time.Now()).String())
+	for i := 1; i <= 5; i ++ {
+		pc, _, line, ok := runtime.Caller(i)
+		f := runtime.FuncForPC(pc)
+		if ok && f.Name() != "runtime.goexit" {
+			color.Green.Println("Caller Skip" + strconv.Itoa(i) + ": " + f.Name() + ":" + strconv.Itoa(line) + "")
+		} else {
+			break
+		}
+	}
+	color.Green.Println("----------------------------------------------------------------------------------------------------")
+	color.Green.Println(message)
+	color.Green.Println("----------------------------------------------------------------------------------------------------")
+	fmt.Println()
 }
 
 func Info(message ...interface{}) {
-	fmt.Println(White("--------------Info Start--------------"))
-	fmt.Println(White(message))
-	fmt.Println(White("--------------Info End--------------"))
+	fmt.Println()
+	color.White.Println("------------------------------------------------INFO------------------------------------------------")
+	color.White.Println("Time: " + timeutl.Time(time.Now()).String())
+	for i := 1; i <= 5; i ++ {
+		pc, _, line, ok := runtime.Caller(i)
+		f := runtime.FuncForPC(pc)
+		if ok && f.Name() != "runtime.goexit" {
+			color.White.Println("Caller Skip" + strconv.Itoa(i) + ": " + f.Name() + ":" + strconv.Itoa(line) + "")
+		} else {
+			break
+		}
+	}
+	color.White.Println("----------------------------------------------------------------------------------------------------")
+	color.White.Println(message)
+	color.White.Println("----------------------------------------------------------------------------------------------------")
+	fmt.Println()
 }
 
 func Warning(message ...interface{}) {
-	fmt.Println(Yellow("--------------Warning Start--------------"))
-	fmt.Println(Yellow(message))
-	fmt.Println(Yellow("--------------Warning End--------------"))
+	fmt.Println()
+	color.Yellow.Println("------------------------------------------------WARN------------------------------------------------")
+	color.Yellow.Println("Time: " + timeutl.Time(time.Now()).String())
+	for i := 1; i <= 5; i ++ {
+		pc, _, line, ok := runtime.Caller(i)
+		f := runtime.FuncForPC(pc)
+		if ok && f.Name() != "runtime.goexit" {
+			color.Yellow.Println("Caller Skip" + strconv.Itoa(i) + ": " + f.Name() + ":" + strconv.Itoa(line) + "")
+		} else {
+			break
+		}
+	}
+	color.Yellow.Println("----------------------------------------------------------------------------------------------------")
+	color.Yellow.Println(message)
+	color.Yellow.Println("----------------------------------------------------------------------------------------------------")
+	fmt.Println()
 }
